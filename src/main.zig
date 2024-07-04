e50dbd7cde64cf120b0f338760bf6adbac298964
Ryp <thibault.schueller@protonmail.com>
2024-07-04 09:30:42 +0300

    matrix: first working generic MkN mat mul

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const assert = std.debug.assert;
+
+const matrix = @import("matrix.zig");
+const Matrix = matrix.Matrix;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -7,16 +11,18 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     // const allocator = std.heap.page_allocator;
 
-    const m1 = f32_4x4{ .data = try allocator.alloc(f32, 4 * 4) };
+    const m1 = Matrix(f32, 4, 4){ .data = try allocator.alloc(f32, 4 * 4) };
     defer allocator.free(m1.data);
 
-    const m2 = f32_4x4{ .data = try allocator.alloc(f32, 4 * 4) };
+    var m2 = Matrix(f32, 8, 4){ .data = try allocator.alloc(f32, 4 * 8) };
     defer allocator.free(m2.data);
 
     // Identity matrix
-    for (m1.data, m2.data) |*elt, *elt2| {
+    for (m1.data) |*elt| {
         elt.* = 0.0;
-        elt2.* = 0.0;
+    }
+    for (m2.data) |*elt| {
+        elt.* = 1.0;
     }
 
     m1.data[0] = 1.0;
@@ -24,78 +30,38 @@ pub fn main() !void {
     m1.data[10] = 1.0;
     m1.data[15] = 1.0;
 
-    m2.data[0] = 1.0;
-    m2.data[5] = 2.0;
-    m2.data[10] = 3.0;
-    m2.data[15] = 4.0;
+    m1.data[2] = 2.0;
+    m1.data[6] = 2.0;
+    m1.data[11] = 2.0;
+    m1.data[1] = 2.0;
 
-    for (m1.data, 0..) |elt, i| {
-        std.debug.print("m[{}] = {}\n", .{ i, elt });
-    }
+    m2.data[0] = 1.0;
+    m2.data[5] = 1.0;
+    m2.data[10] = 1.0;
+    m2.data[15] = 1.0;
+
+    m1.print();
+
+    m2.print();
 
     const v: [4]f32 = .{ 1, 2, 3, 4 };
 
-    const rslt = mul(f32, m1, v);
+    const rslt = m1.mul_vec(v);
 
-    for (rslt, 0..) |elt, i| {
-        std.debug.print("v[{}] = {}\n", .{ i, elt });
+    for (0..4) |i| {
+        std.debug.print("v[{}] = {}\n", .{ i, rslt[i] });
     }
 
-    const r = Matrix(m1.subtype, m2.columns, m1.rows){ .data = try allocator.alloc(f32, 4 * 4) };
+    const r = Matrix(m1.scalar_type, m2.columns, m1.rows){ .data = try allocator.alloc(f32, 4 * 8) };
     defer allocator.free(r.data);
 
     for (r.data) |*elt| {
         elt.* = 0.0;
     }
 
-    mul_mat(m1, m2, r);
+    m2.transpose_storage_copy();
 
-    for (r.data, 0..) |elt, i| {
-        std.debug.print("m[{}] = {}\n", .{ i, elt });
-    }
-}
+    m1.mul_mat(m2.columns, m2, r);
 
-const f32_4x4 = Matrix(f32, 4, 4);
-
-fn Matrix(comptime T: type, columns: usize, rows: usize) type {
-    return struct {
-        data: []T,
-        comptime columns: usize = columns,
-        comptime rows: usize = rows,
-        comptime subtype: type = T,
-    };
-}
-
-fn dot(comptime T: type, v1: @Vector(4, T), v2: @Vector(4, T)) T {
-    return @reduce(.Add, v1 * v2);
-}
-
-fn mul(comptime T: type, m: f32_4x4, v: [4]f32) [4]f32 {
-    return .{
-        dot(T, m.data[0..4].*, v),
-        dot(T, m.data[4..8].*, v),
-        dot(T, m.data[8..12].*, v),
-        dot(T, m.data[12..16].*, v),
-    };
-}
-
-fn mul_mat(m1: f32_4x4, m2t: f32_4x4, mat_d: f32_4x4) void {
-    comptime {
-        std.debug.assert(m1.subtype == m2t.subtype);
-        std.debug.assert(m1.columns == m2t.rows);
-        std.debug.assert(mat_d.rows == m1.rows);
-        std.debug.assert(mat_d.columns == m2t.columns);
-    }
-
-    for (0..mat_d.rows) |index_row| {
-        const offset_a = index_row * m1.columns;
-        const row_a = m1.data[offset_a..][0..m1.columns];
-
-        for (0..mat_d.columns) |index_col| {
-            const offset_b = index_col * m2t.columns; // FIXME
-            const col_b = m2t.data[offset_b..][0..m2t.columns]; // FIXME treated as col/major here
-
-            mat_d.data[index_row + index_col * m2t.columns] = dot(mat_d.subtype, row_a.*, col_b.*);
-        }
-    }
+    r.print();
 }
---
+++ src/matrix.zig
@@ -0,0 +1,108 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+const Storage = enum(u1) {
+    ColumnMajor,
+    RowMajor,
+};
+
+pub fn Matrix(comptime T: type, columns: usize, rows: usize) type {
+    return struct {
+        comptime scalar_type: type = T,
+        comptime columns: usize = columns,
+        comptime rows: usize = rows,
+        storage: Storage = .RowMajor,
+        data: []T,
+
+        fn index_flat_storage(storage: Storage, col_index: usize, row_index: usize) usize {
+            return switch (storage) {
+                .RowMajor => row_index * columns + col_index,
+                .ColumnMajor => col_index * rows + row_index,
+            };
+        }
+
+        fn index_flat(self: @This(), col_index: usize, row_index: usize) usize {
+            return index_flat_storage(self.storage, col_index, row_index);
+        }
+
+        fn get_row(self: @This(), row_index: usize) @Vector(columns, T) {
+            assert(self.storage == .RowMajor);
+            const offset = row_index * columns;
+            return self.data[offset..][0..columns].*;
+        }
+
+        fn get_column(self: @This(), col_index: usize) @Vector(rows, T) {
+            assert(self.storage == .ColumnMajor);
+            const offset = col_index * rows;
+            return self.data[offset..][0..rows].*;
+        }
+
+        fn at(self: @This(), col_index: usize, row_index: usize) T {
+            const index = index_flat(self, col_index, row_index);
+            return self.data[index];
+        }
+
+        pub fn mul_vec(self: @This(), vector: @Vector(rows, T)) @Vector(rows, T) {
+            var result: @Vector(rows, T) = undefined;
+
+            for (0..rows) |row_index| {
+                result[row_index] = dot(T, rows, self.get_row(row_index), vector);
+            }
+
+            return vector;
+        }
+
+        pub fn mul_mat(self: @This(), comptime n: usize, m2: Matrix(T, n, columns), mat_d: Matrix(T, n, rows)) void {
+            assert(self.storage == .RowMajor);
+            assert(m2.storage == .ColumnMajor);
+
+            for (0..mat_d.rows) |row_index| {
+                const row_a = self.get_row(row_index);
+
+                for (0..mat_d.columns) |col_index| {
+                    const col_b = m2.get_column(col_index);
+
+                    const index = mat_d.index_flat(col_index, row_index);
+
+                    mat_d.data[index] = dot(T, columns, row_a, col_b);
+                }
+            }
+        }
+
+        pub fn transpose_storage_copy(self: *@This()) void {
+            const new_storage: Storage = if (self.storage == .ColumnMajor) .RowMajor else .ColumnMajor;
+            var transpose: [columns * rows]T = undefined;
+
+            for (0..rows) |row| {
+                for (0..columns) |column| {
+                    const flat_index_transposed = index_flat_storage(new_storage, column, row);
+
+                    // Read with inverse mapping
+                    transpose[flat_index_transposed] = self.at(column, row);
+                }
+            }
+
+            std.mem.copyForwards(T, self.data, &transpose);
+
+            self.storage = new_storage;
+        }
+
+        pub fn print(self: @This()) void {
+            std.debug.print("mat {}x{} = [\n", .{ columns, rows });
+
+            for (0..rows) |row_index| {
+                std.debug.print("  [ ", .{});
+                for (0..columns) |col_index| {
+                    std.debug.print("{:<4} ", .{self.at(col_index, row_index)});
+                }
+                std.debug.print("]\n", .{});
+            }
+
+            std.debug.print("]\n", .{});
+        }
+    };
+}
+
+pub fn dot(comptime T: type, comptime size: usize, v1: @Vector(size, T), v2: @Vector(size, T)) T {
+    return @reduce(.Add, v1 * v2);
+}
