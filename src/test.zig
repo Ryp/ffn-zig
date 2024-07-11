64f51bb69a168efbcf70440c5b8e4e4a08dead93
Thibault Schueller <1625198+Ryp@users.noreply.github.com>
2024-07-11 21:01:40 +0300

    matrix: move main setup into test

--- src/test.zig
+++ src/test.zig
@@ -34,3 +34,69 @@ test "basic" {
 
     try testing.expect(idx1_0 != idx1_0_t);
 }
+
+test "matmul" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+    // const allocator = std.heap.page_allocator;
+
+    const M = 8192 / 2;
+    const N = 128;
+    const k = 8192 / 2;
+
+    const flops = 2 * M * N * k;
+    const bytes = 2 * (M * k + N * k + N * M);
+    const ai = @as(f32, @floatFromInt(flops)) / @as(f32, @floatFromInt(bytes));
+    _ = ai;
+
+    // std.debug.print("Arithmetic intensity = {}\n", .{ai});
+
+    const m1 = Matrix(f32, k, M){ .data = try allocator.alloc(f32, k * M) };
+    defer allocator.free(m1.data);
+
+    var m2 = Matrix(f32, N, k){ .data = try allocator.alloc(f32, N * k) };
+    defer allocator.free(m2.data);
+
+    // Identity matrix
+    for (m1.data) |*elt| {
+        elt.* = 0.0;
+    }
+
+    for (m2.data) |*elt| {
+        elt.* = 1.0;
+    }
+
+    m1.data[0] = 1.0;
+    m1.data[5] = 1.0;
+    m1.data[10] = 1.0;
+    m1.data[15] = 1.0;
+
+    m1.data[2] = 2.0;
+    m1.data[6] = 2.0;
+    m1.data[11] = 2.0;
+    m1.data[1] = 2.0;
+
+    m2.data[0] = 1.0;
+    m2.data[5] = 1.0;
+    m2.data[10] = 1.0;
+    m2.data[15] = 1.0;
+
+    // m1.print();
+
+    // m2.print();
+
+    const r = Matrix(f32, N, M){ .data = try allocator.alloc(f32, N * M) };
+    defer allocator.free(r.data);
+
+    for (r.data) |*elt| {
+        elt.* = 0.0;
+    }
+
+    m2.transpose_storage_copy();
+
+    m1.mul_mat(m2.columns, m2, r);
+
+    // r.print();
+}
